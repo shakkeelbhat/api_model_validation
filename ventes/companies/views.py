d9ce6b23d60419115ddc7afc18133b134f5aba51
shakkeelbhat <shakkeelbhat@gmail.com>
2023-10-14 23:05:44 +0530

    views basic added

--- ventes/companies/views.py
+++ ventes/companies/views.py
@@ -1,17 +1,19 @@
 from django.shortcuts import render
 from companies.serializers import CompanySerializer
-from companies.serializers import CompanySerializer
+from rest_framework.views import APIView
 
 
+class CUDApiView(APIView):
+    def post(self,request):
+        pass
 
-class CUDApiView():
-    pass
+class GetCompanyByIDApiView(APIView):
+    def get(self,request,id):
+        pass
 
-class GetCompanyByIDApiView():
-    pass
-
-class GetAllCompaniesApiView():
-    pass 
+class GetAllCompaniesApiView(APIView):
+    def get(self,request) :
+        pass
 
 
 
